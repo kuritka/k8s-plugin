9d9b3add3dbd4f25a75fb5ec80a144aa3509e2ed
kuritka <kuritka@gmail.com>
2020-08-26 22:16:38 +0200

    remove useless information

--- status/status.go
+++ status/status.go
@@ -13,7 +13,6 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 )
 
 //StatusInfo command structure
@@ -38,41 +37,12 @@ func New(options Options) *Info {
 
 //Run runs the command implementation
 func (s *Info) Run() error {
-	fmt.Println(s.options.Context.K8s.RawConfig.Contexts)
-	fmt.Println(s.options.Context.K8s.RawConfig.CurrentContext)
 	printGslb(s.options.Context.K8s.DynamicConfig)
+	fmt.Println(s.options.Context.K8s.RawConfig.CurrentContext)
 	e := s.options.Context.K8s.SwitchContext("kind-test-gslb2")
-	defer func() {
-		fmt.Println("Switch context back...")
-		err := s.options.Context.K8s.TearDown()
-		guard.FailOnError(err, "unable to switch back context back. Plugin created .lock file next to your kube config. Please remove that file manually")
-	}()
-
 	guard.FailOnError(e, "")
 	fmt.Println(s.options.Context.K8s.RawConfig.CurrentContext)
-	for k := range s.options.Context.K8s.RawConfig.Clusters {
-		cluster := emoji.Sprint(" :hamburger: ", k)
-		fmt.Println(cluster)
-	}
-	cs, err := kubernetes.NewForConfig(s.options.Context.K8s.ClientConfig)
-	if err != nil {
-		return err
-	}
-
-	host := emoji.Sprint(" :sushi: ", s.options.Context.K8s.ClientConfig.Host)
-	fmt.Println(host)
-
-	//package api from k8gb import here...
-	ing, err := cs.NetworkingV1beta1().Ingresses(s.options.Namespace).List(metav1.ListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, n := range ing.Items {
-		logger.Info().Msgf("%s %s", n.ClusterName, n.Name)
-	}
 	printGslb(s.options.Context.K8s.DynamicConfig)
-
 	return nil
 }
 
